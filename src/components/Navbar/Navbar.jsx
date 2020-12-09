e7536890556a061c1cd83ccb82dbce8c4eb629b9
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-12-09 20:32:10 +0200

    built navbar more

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,6 +12,14 @@ const Navbar = () => {
                         <img src={logo} alt="ShoppingSite" className={classes.image} height="25px" />
                         ShoppingSite
                     </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                        <IconButton arial-label="Show cart items" color="inherit">
+                            <Badge badgeContent={2} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
                 </Toolbar>
             </AppBar>
         </>
