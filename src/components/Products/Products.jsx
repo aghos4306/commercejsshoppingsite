36131c26e38b0fd021f3376fa9c5162c5861f751
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-12-09 20:49:30 +0200

    style effect added to products to push products down

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
-//import { ShortTextSharp } from '@material-ui/icons';
 import Product from './Product/Product';
+
+import useStyles from './styles';
 
 const products = [
     { id: 1, name: 'Shoes', description: 'sports shoe', price: '£14', image: '' },
@@ -10,8 +11,10 @@ const products = [
 ]
 
 const Products = () => {
+    const classes = useStyles()
     return (
-        <main>
+        <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
