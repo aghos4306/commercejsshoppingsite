a440c81cd5e73ea6d63b471b3648a5089a737264
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-12-10 03:37:26 +0200

    display cart data count on ui

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const App = () => {
 
     return (
         <div>
-            <Navbar />
+            <Navbar totalItems={cart.total_items} />
             <Products products={products} onAddToCart={handleAddToCart}/>
         </div>
     )
