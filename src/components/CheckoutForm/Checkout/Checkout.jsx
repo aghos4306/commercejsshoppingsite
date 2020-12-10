209ba62e06c24bdf22b5141c9482f172381d2018
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-12-11 01:17:50 +0200

    style classes added

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
+import useStyles from './styles';
+
 const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = () => {
+    const classes = useStyles();
     return (
         <>
             <div className={classes.toolbar} />
