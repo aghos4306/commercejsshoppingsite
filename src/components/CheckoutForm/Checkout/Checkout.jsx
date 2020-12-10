c19f6b2a73e67186bcbb270ef9dbcca56d91bccd
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-12-11 01:53:07 +0200

    skeleton of three pieces of components needed for stepping the form

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 import useStyles from './styles';
 
@@ -9,8 +11,14 @@ const Checkout = () => {
     const classes = useStyles();
     const [activeStep, setActiveStep] = useState(0);
 
+    const Confirmation = () => {
+        <div>
+            Confirmation
+        </div>
+    }
+
     const Form = () => activeStep === 0
-        ? <AddressFrom />
+        ? <AddressForm />
         : <PaymentForm />
 
     return (
