ce0a76f1e40970bca8c9bd4391b2f59ee76738d1
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-12-10 02:56:57 +0200

    create cart state in App.js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,29 @@ import { commerce } from './components/lib/commerce';
 import { Products, Navbar } from './components'
 
 const App = () => {
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
 
+    const [cart, setCart] = useState({});
+
+    //handle fetch products
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
         setProducts(data);
     }
 
+    //handle adding products to cart
+    const fetchCart = async () => {
+        const cart = await commerce.cart.retrieve()
+        setCart(cart);
+        //setCart(await commerce.cart.retrieve());
+    }
+
     useEffect(() => {
         fetchProducts();
+        fetchCart();
     }, []);
     //console.log(products);
+    console.log(cart);
 
     return (
         <div>
