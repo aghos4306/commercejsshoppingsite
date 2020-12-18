5b2484d64ba3a6c8ad80b3af3417606dffdd6e81
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-12-18 04:45:17 +0200

    render form component only if checkoutToken is available

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -24,7 +24,7 @@ const Checkout = ({ cart }) => {
             }
         }
         generateToken();
-    }, []);
+    }, [cart]);
 
     const Confirmation = () => (
         <div>
@@ -49,7 +49,7 @@ const Checkout = ({ cart }) => {
                             </Step>
                         ))}
                     </Stepper>
-                    {activeStep === steps.length ? <Confirmation /> : <Form />} 
+                    {activeStep === steps.length ? <Confirmation /> : checkoutToken && <Form />} 
                 </Paper>
             </main>
         </>
